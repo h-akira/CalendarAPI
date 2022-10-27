67a19ded89971247b1883dc24d6c28c888392c20
h-akira <79441679+h-akira@users.noreply.github.com>
2022-10-27 10:58:12 +0000

    add color option

--- lib/templates.py
+++ lib/templates.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def basic(summary,start_time,end_time,location=None, description=None, timeZone='Japan'):
+def basic(summary,start_time,end_time,location=None, description=None, colorId=0, timeZone='Japan'):
   event = {
     'summary': summary,
     'location': location,
@@ -12,6 +12,7 @@ def basic(summary,start_time,end_time,location=None, description=None, timeZone=
     'end': {
       'dateTime': end_time.isoformat(),
       'timeZone': timeZone,
-    }
+    },
+    'colorId': colorId
   }
   return event
--- sample/input_sample.py
+++ sample/input_sample.py
@@ -3,10 +3,10 @@
 # templates.pyをimportして利用できる．各関数は辞書型を返す．
 ### Usage of templates ###
 # events.append(tempalates.basic(タイトル，開始時刻[datetime型]，終了時刻[同]，
-# location=場所，description=説明，timeZone=タイムゾーン[デフォルトはJapan]))
+# location=場所，description=説明，colorId=カラー[0から11，str可], timeZone=タイムゾーン[デフォルトはJapan]))
 
 ### Sample ###
 import templates
 from datetime import datetime as dt
 events.append(templates.basic("テスト1",dt(2022,11,6,11,00),dt(2022,11,6,13,00)))
-events.append(templates.basic("テスト2",dt(2022,11,7,11,00),dt(2022,11,7,13,00)))
+events.append(templates.basic("テスト2",dt(2022,11,7,11,00),dt(2022,11,7,13,00),colorId=4))
